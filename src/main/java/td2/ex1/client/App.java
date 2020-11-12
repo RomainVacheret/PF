9d4de9f50b652d3df219e3edadeeb0a9ad444de1
Romain Vacheret <romain.vacheret@gmail.com>
2020-11-12 09:57:53 +0100

    Short version addition

--- src/main/java/td2/ex1/client/App.java
+++ src/main/java/td2/ex1/client/App.java
@@ -9,6 +9,7 @@ import td2.ex1.api.ToString;
 public class App {
     public static void main(String[] args) {
         //Q1
+        // Version longue
         Somme<Integer> sommeInteger = new Somme<Integer>() {
             @Override
 			public Integer somme(Integer a, Integer b) {
@@ -46,6 +47,22 @@ public class App {
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
+
+        // Version courte
+        Somme<Integer> sommeInteger2 = (x, y) -> x + y;
+        Somme<Double> sommeDouble2 = (x, y) -> x + y;
+        Somme<Long> sommeLong2 = (x, y) -> x + y;
+        Somme<String> sommeString2 = (x, y) -> x + y;
+        
+        int a2 = sommeInteger2.somme(1, 2);
+        double b2 = sommeDouble2.somme(1.1, 2.2);
+        long c2 = sommeLong2.somme((long) 1, (long) 2);
+        String d2 = sommeString2.somme("A", "B");
+
+        System.out.println(a2);
+        System.out.println(b2);
+        System.out.println(c2);
+        System.out.println(d2);
 
 
         // Q2
